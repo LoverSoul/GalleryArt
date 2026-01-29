a50982fe341a2c41119cf51c89c1b9a2b26a2a39
TrueHumanSoul <32673139+LoverSoul@users.noreply.github.com>
2026-01-29 13:09:45 +0300

    text color hotfix

--- Assets/Scripts/GameScreen/ButtonSelect.cs
+++ Assets/Scripts/GameScreen/ButtonSelect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace GA.GameScreen
@@ -7,10 +8,12 @@ namespace GA.GameScreen
     public class ButtonSelect : MonoBehaviour
     {
         [SerializeField] private GameObject selectedVisual;
-
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private Color selectedColor;
         public void Select(bool isSelected) 
         {
             selectedVisual.SetActive(isSelected);
+            text.color = isSelected ? selectedColor : Color.black;
         }
     }
 }
